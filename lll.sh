c9a47a0565fd3b3eb128b869cc0b120eed07d379
jelena.pejicic <jelena.pejicic@displaylink.com>
2020-07-06 19:28:02 +0200

    '...'

--- lll.sh
+++ lll.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-echo "upisi neki tekst u fajl JELENAAAA" > sss.txt
+echo "upisi neki tekst u fajl" > sss.txt
+COPY package.json /usr/src/app/
+COPY package-lock.json /usr/src/app/
+COPY lll.sh /usr/src/app
 #npm run eslint -f cccc.txt
